69d7c7cd29c7687d5570b462784b52084cb064a3
Muhammed Anas <anask2233@gmail.com>
2025-05-16 17:14:52 +0530

    base url change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   demo: 'saas', // other possible options are creative and modern
   GOOGLE_MAPS_API_KEY: 'AIzaSyDpgQMpcfx1QU-8SM-ljcgjG4xrYtIYby4',
-  baseUrl: 'https://iprulers.zaap.life', 
+     baseUrl:'https://iprulerslive.com',
+
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   demo: 'saas', // other possible options are creative and modern
   GOOGLE_MAPS_API_KEY: 'AIzaSyDpgQMpcfx1QU-8SM-ljcgjG4xrYtIYby4',
-  baseUrl: 'https://iprulers.zaap.life', 
+   baseUrl:'https://iprulerslive.com',
   // baseUrl: 'http://localhost:5000', 
 
 };
